8351f5580d64ed03a1a10da22a54f6c71e5c5233
Nikita Krapivin <alienoom@yandex.ru>
2021-12-22 22:31:02 +0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -192,16 +192,20 @@ int _main(struct thread *td) {
   initSysUtil();
   initNetwork();
   initPthread();
+  initModule();
   ok = -1;
   
   printf_notification("Starting...");
   threadRet = NULL;
   
   // load bgft module and resolve stuff...
+  printf_debug("[main] preinit...\n");
   libBgft = sceKernelLoadStartModule("libSceBgft.sprx", 0, 0, 0, NULL, NULL);
+  printf_debug("[main] bgft = 0x%X\n", libBgft);
   RESOLVE(libBgft, sceBgftInitialize);
   RESOLVE(libBgft, sceBgftDownloadRegisterTaskByStorage);
   RESOLVE(libBgft, sceBgftDownloadStartTask);
+  printf_debug("[main] bgft resolve ok, 0x%p\n", sceBgftInitialize);
   
   struct bgft_init_params ip = {
     .mem = mmap(NULL, 0x100000, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0),
